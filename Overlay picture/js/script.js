cc81c21b642b70bd1b33d07c32fbb4079d5d3191
Mikhail Kirichenko <66267316+bigmike94@users.noreply.github.com>
2020-12-26 03:03:58 +0400

    Add files via upload

---
+++ Overlay picture/js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+class PictureOverlay{
+	constructor(wrapper, overlayDiv, line, circle){
+		this.circle = document.getElementById(circle);
+		this.wrapper = document.getElementById(wrapper);
+		this.overlayDiv = document.getElementById(overlayDiv);
+		this.line = document.getElementById(line);
+	}
+	dropCircle(event){
+		let x = this.checkCoordinates(event);
+		this.line.style.left = x+"px";
+		this.overlayDiv.style.width = x+"px";
+	}
+	checkCoordinates(event){
+		let x = event.clientX - this.wrapper.offsetLeft;
+		if (x>=this.wrapper.clientWidth) x = this.wrapper.clientWidth;
+		else if (x<0) x=0;
+		return x;
+	}
+	overlay(event) {
+		let x = this.checkCoordinates(event);
+		this.line.style.left = x+"px";
+		this.overlayDiv.style.width = x+"px";
+	}
+
+}
+let pictureOverlay = new PictureOverlay("pictures-wrapper", "overlay", "line", "circle");
+
+
+pictureOverlay.circle.addEventListener("drag", function(event){
+	pictureOverlay.overlay(event);
+});
+
+pictureOverlay.circle.addEventListener("dragend", function(event){
+	pictureOverlay.dropCircle(event);
+});
